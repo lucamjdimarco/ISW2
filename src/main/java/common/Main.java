605a57f740bc757a233a6fa21452f97a130bc1e3
lucamjdimarco <lucadm24@gmail.com>
2025-01-07 15:31:30 +0100

    Fix Main.java

--- src/main/java/common/Main.java
+++ src/main/java/common/Main.java
@@ -16,53 +16,52 @@ import java.util.List;
 
 import static weka.WekaController.calculateWeka;
 
-
 public class Main {
-
-
 
     public static void main(String[] args) {
         try {
-
-            String project = "SYNCOPE";
-            String path = ConfigLoader.getSyncopePath();
-
-            List<Release> releases;
-            List<Ticket> tickets;
-            List<RevCommit> commits;
-
-            releases = JiraRelease.getRelease(project);
-            tickets = JiraTicket.getTickets(project);
-
-            commits = GitController.retrieveCommits(path);
-
-            GitController.calculateMetric(releases, path);
-
-            JiraTicket.commitsOfTheticket(commits, tickets);
-            JiraTicket.removeTicketWithoutCommit(tickets);
-            JiraTicket.fixTicketList(tickets);
-
-            //PROPORTION
-            Collections.reverse(tickets);
-
-            Proportion.getProportion(tickets, project);
-            for(Ticket ticket: tickets){
-                JiraTicket.calculateAV(ticket);
-            }
-
-            String subpath = "fileCSV/";
-
-            WriteCSV.writeReleasesForWalkForward(releases, tickets, subpath + project + "/training/file", subpath + project + "/testing/file", path);
-
-            WekaController.convertAllCsvInFolder(subpath + project + "/training");
-            WekaController.convertAllCsvInFolder(subpath + project + "/testing");
-
-            calculateWeka(project, releases.size());
+            runProjectPipeline("BOOKKEEPER", ConfigLoader.getBookkeeperPath());
+            runProjectPipeline("SYNCOPE", ConfigLoader.getSyncopePath());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+
+
+    private static void runProjectPipeline(String project, String path) throws Exception {
+        List<Release> releases;
+        List<Ticket> tickets;
+        List<RevCommit> commits;
+
+        releases = JiraRelease.getRelease(project);
+        tickets = JiraTicket.getTickets(project);
+
+        commits = GitController.retrieveCommits(path);
+
+        GitController.calculateMetric(releases, path);
+
+        JiraTicket.commitsOfTheticket(commits, tickets);
+        JiraTicket.removeTicketWithoutCommit(tickets);
+        JiraTicket.fixTicketList(tickets);
+
+        //PROPORTION
+        Collections.reverse(tickets);
+
+        Proportion.getProportion(tickets, project);
+        for(Ticket ticket: tickets){
+            JiraTicket.calculateAV(ticket);
+        }
+
+        String subpath = "fileCSV/";
+
+        WriteCSV.writeReleasesForWalkForward(releases, tickets, subpath + project + "/training/file", subpath + project + "/testing/file", path);
+
+        WekaController.convertAllCsvInFolder(subpath + project + "/training");
+        WekaController.convertAllCsvInFolder(subpath + project + "/testing");
+
+        calculateWeka(project, releases.size());
 
     }
 }
